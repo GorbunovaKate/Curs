63876a610c48c0ebf286c18e4116c6538eb87bdb
Gorbunova Ekaterina <login.kate1.pass@gmail.com>
2023-01-13 14:02:02 +0700

    Splash Screen

--- app/src/main/java/com/example/developercurs/MainActivity.kt
+++ app/src/main/java/com/example/developercurs/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.developercurs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
@@ -12,11 +13,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.developercurs.ui.theme.DeveloperCursTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel:MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{
+                viewModel.isLoading.value
+            }
+        }
         setContent {
                // Navigation()
             Bottom_Navigation()
